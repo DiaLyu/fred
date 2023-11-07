0946084c2971c446b9b6293d85fc12652d25b6b8
Diana <05idianka6282@gmail.com>
2023-11-07 16:01:07 +0400

    add component descr8

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,7 +3,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Kint\Kint;
 // E05idiip\Helpers\Debug::test();
 
-echo "зашло сюда";
 $eventManager = \Bitrix\Main\EventManager::getInstance();
 $eventManager->addEventHandler("iblock", "OnAfterIBlockElementUpdate", ['E05idiip\\Socials\\SocialNetworks', 'updateEventNetwork']);
 // $eventManager->addEventHandler("iblock", "OnAfterIBlockElementAdd", ['E05idiip\\Socials\\SocialNetworks', 'addEventNetwork']);
