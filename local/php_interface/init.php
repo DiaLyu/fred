98082d4ea0a6ad8c88917b7096c9d7754fa7a969
Diana <05idianka6282@gmail.com>
2023-11-07 11:41:08 +0400

    comment code

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,9 +3,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Kint\Kint;
 // E05idiip\Helpers\Debug::test();
 
-$eventManager = \Bitrix\Main\EventManager::getInstance();
-$eventManager->addEventHandler("iblock", "OnAfterIBlockElementUpdate", ['E05idiip\\Socials\\SocialNetworks', 'updateEventNetwork']);
-$eventManager->addEventHandler("iblock", "OnAfterIBlockElementAdd", ['E05idiip\\Socials\\SocialNetworks', 'addEventNetwork']);
+// $eventManager = \Bitrix\Main\EventManager::getInstance();
+// $eventManager->addEventHandler("iblock", "OnAfterIBlockElementUpdate", ['E05idiip\\Socials\\SocialNetworks', 'updateEventNetwork']);
+// $eventManager->addEventHandler("iblock", "OnAfterIBlockElementAdd", ['E05idiip\\Socials\\SocialNetworks', 'addEventNetwork']);
 
 
 ?>
